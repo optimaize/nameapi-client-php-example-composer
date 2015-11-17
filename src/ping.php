73c90459237205cf6bf189a226deb3656270dc02
Fabian Kessler <fabian.kessler@optimaize.com>
2015-11-17 06:23:07 +0200

    Connecting to host preview-api.nameapi.org because version 5.0 is not live yet.

--- src/ping.php
+++ src/ping.php
@@ -4,6 +4,7 @@
  */
 
 require '../vendor/autoload.php';
+use org\nameapi\client\services\Host;
 use org\nameapi\ontology\input\context\Context;
 use org\nameapi\ontology\input\context\Priority;
 use org\nameapi\client\services\ServiceFactory;
@@ -14,7 +15,7 @@ define('NAMEAPI_API_KEY', 'your-api-key'); //grab one from nameapi.org
 $context = Context::builder()
     ->priority(Priority::REALTIME())
     ->build();
-$serviceFactory = new ServiceFactory(NAMEAPI_API_KEY, $context);
+$serviceFactory = new ServiceFactory(NAMEAPI_API_KEY, $context, Host::http('preview-api.nameapi.org'), '5.0');
 $pingService = $serviceFactory->systemServices()->ping();
 
 echo "The server says: " . $pingService->ping();
